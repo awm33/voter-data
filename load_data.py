9f88626f7cc7cef61b07aaca83a3f7182eba2473
Andrew Madonna <andrew.w.madonna@gmail.com>
2016-12-28 18:55:02 -0500

    Use json table schema, add poltical parties, prep geo fields

--- load_data.py
+++ load_data.py
@@ -19,15 +19,21 @@ def load_models():
                 models[filename.replace('.yml','')] = model
     return models
 
-def generate_field_sql(field_name, field_def):
-    if field_def['type'] == 'integer':
+def generate_field_sql(field_def):
+    if 'postgresType' in field_def:
+        sql_type = field_def['postgresType']
+    elif field_def['type'] == 'integer':
         sql_type = 'integer'
-    elif field_def['type'] == 'string' and 'format' not in field_def:
-        sql_type = 'text'
-    elif field_def['type'] == 'string' and field_def['format'] == 'date-time':
+    elif field_def['type'] == 'number':
+        sql_type = 'double precision'
+    elif field_def['type'] == 'object':
+        sql_type = 'jsonb'
+    elif field_def['type'] == 'date':
         sql_type = 'timestamp'
+    elif field_def['type'] == 'string':
+        sql_type = 'text'
 
-    return '"' + field_name + '" ' + sql_type + ','
+    return '"' + field_def['name'] + '" ' + sql_type + ','
 
 def generate_primary_key_sql(primary_key):
     if isinstance(primary_key, list):
@@ -38,11 +44,10 @@ def generate_primary_key_sql(primary_key):
 def create_table(cur, model_name, model):
     print('Creating table for: ' + model_name)
 
-    properties = model['properties']
-    ordered_property_keys = sorted(model['properties'], key = lambda x: properties[x]['order'])
+    fields = model['fields']
     sql = 'CREATE TABLE "' + model_name + '" ('
-    for field_name in ordered_property_keys:
-        sql += generate_field_sql(field_name, properties[field_name])
+    for field in fields:
+        sql += generate_field_sql(field)
     if 'primaryKey' in model:
         sql += generate_primary_key_sql(model['primaryKey'])
     else:
@@ -63,12 +68,12 @@ def drop_tables(cur, models):
         result = cur.execute('DROP TABLE IF EXISTS "' + model_name + '"')
 
 def format_registration_dates(row, model):
-    for property_name in model['properties']:
-        if property_name == 'snapshot_date':
+    for i in range(0, len(model['fields'])):
+        field = model['fields'][i]
+        if field['name'] == 'snapshot_date':
             continue
-        property = model['properties'][property_name]
-        if property['type'] == 'string' and 'format' in property and property['format'] == 'date-time':
-            position = property['order'] - 1
+        if field['type'] == 'date':
+            position = i - 1 # id field is 0
             if row[position] != '':
                 row[position] = arrow.get(row[position],'MM/DD/YYYY').format('YYYY-MM-DD HH:mm:ss')
 
@@ -83,11 +88,16 @@ def load_file(cur, model_name, model, file, snapshot_date):
             row.append(snapshot_date_formatted)
             output_csv.writerow(row)
         output.seek(0)
-        cur.copy_expert('COPY ' + model_name + ' FROM STDIN WITH CSV', output)
+
+        cols = ','.join(list(
+                map(lambda x: x['name'],
+                    filter(lambda x: 'importSkip' not in x or x['importSkip'] != True,
+                        model['fields']))))
+        cur.copy_expert('COPY ' + model_name + ' (' + cols + ') FROM STDIN WITH CSV', output)
 
 def get_model_by_file_type(models, file_type):
     for model_name in models:
-        if models[model_name]['exportFileType'] == file_type:
+        if 'exportFileType' in models[model_name] and models[model_name]['exportFileType'] == file_type:
             return model_name, models[model_name]
 
 def load_data(cur, models, data_path):
@@ -101,6 +111,22 @@ def load_data(cur, models, data_path):
                     model_name, model = get_model_by_file_type(models, file_metadata['file_type'])
                     load_file(cur, model_name, model, file, file_metadata['file_date'])
 
+def load_political_parties(cur, models, political_party_data_file):
+    with open(political_party_data_file, encoding='ISO-8859-1') as file:
+        model = models['political_party']
+        with io.StringIO() as output:
+            input_tsv = csv.reader(file, delimiter='\t')
+            rows = list(input_tsv)
+
+            snapshot_date_formatted = arrow.get(rows[-1][0],'M/D/YYYY').format('YYYY-MM-DD HH:mm:ss')
+
+            output_csv = csv.writer(output)
+            for row in rows[1:-2]: ## skip header and datetime signature at the end of the file
+                row.append(snapshot_date_formatted)
+                output_csv.writerow(row)
+            output.seek(0)
+            cur.copy_expert('COPY political_party FROM STDIN WITH CSV', output)
+
 def get_settings():
     return {
         'database': {
@@ -110,7 +136,8 @@ def get_settings():
             'host': os.getenv('VOTER_DATABASE_HOST', 'localhost'),
             'port': os.getenv('VOTER_DATABASE_PORT', '5432')
         },
-        'data_path': os.getenv('VOTER_DATA_PATH', '/users/amadonna/Downloads/Statewide')
+        'data_path': os.getenv('VOTER_DATA_PATH', '/users/amadonna/Downloads/Statewide'),
+        'political_party_data_file': os.getenv('VOTER_POLITICAL_PARTY_DATA_FILE', '/users/amadonna/Downloads/Political'),
     }
 
 def main():
@@ -121,6 +148,7 @@ def main():
         with conn.cursor() as cur:
             drop_tables(cur, models)
             create_tables(cur, models)
+            load_political_parties(cur, models, settings['political_party_data_file'])
             load_data(cur, models, settings['data_path'])
 
 if __name__ == "__main__":
